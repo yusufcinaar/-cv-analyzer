3c040f605d8a7af984a4190a102cd4d49c51448c
yusufcinaar <yusufcinar011@gmail.com>
2025-06-23 15:46:02 +0300

    Çalışma dizini düzeltildi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
+
+# Projenin kök dizinine geç
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
